54a8e2cff83254e61d599e945140352a03695729
Umifl <114711788+Umifl@users.noreply.github.com>
2022-10-14 16:56:24 +0700

    Add files via upload

---
+++ Dosen.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.8">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Dosen</title>
+</head>
+<body>
+    <h1>Mata Pelajaran</h1>
+    <p><?php echo $MK01; ?></P>
+    <p><?php echo $MK02; ?></P>
+    <p><?php echo $MK03; ?></P>
+</body>
+</html>
